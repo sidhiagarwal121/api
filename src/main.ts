4f85e6b74a14a69a41ac9b50856710b1f43ffe50
sidhiagarwal <sidhiagarwal9839@gmail.com>
2023-11-16 12:47:19 +0530

    update-port-again

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.setGlobalPrefix('api')
 
-  await app.listen(4000);
+  await app.listen(3000);
 }
 bootstrap();
